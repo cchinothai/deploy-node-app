85eb01d38036adc8cfc47673de80623e9ca5f3ae
Cody <cchinothai@gmail.com>
2025-11-12 16:46:36 -0800

    configure eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,17 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, 
-  extends: [
-    "js/recommended", 
-    "plugin:react/recommended"
-  ], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: [
+      "react", 
+      "js"
+    ], 
+    extends: [
+      "js/recommended", 
+      "plugin:react/recommended"
+    ], 
+    languageOptions: { globals: globals.browser } 
+},
   pluginReact.configs.flat.recommended,
 ]);
